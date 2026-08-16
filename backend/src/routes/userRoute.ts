94d76671a24bc28452a3b5b93d0b97be09377763
uksinha-sudo <sinhanikhil366@gmail.com>
2026-08-16 08:29:03 +0530

    Added delete endpoint for user

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -144,3 +144,19 @@ userRouter.put("/update", authMiddleware, async(req, res) => {
         return res.status(500).json({message:"Internal server error, failed to update information"})
     }
 });
+
+userRouter.delete("/delete", authMiddleware, async(req, res) => {
+    try{
+        const deletedUser = await userModel.findByIdAndDelete(req.userId);
+
+        if(!deletedUser){
+            return res.status(404).json({
+                message:"User not found"
+            })
+        }
+        
+        res.status(200).json({message:"Account Deleted"});
+    } catch(error){
+        res.status(500).json({message:"Internal server error, failed to delete account"})
+    }
+})
